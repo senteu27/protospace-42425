81bc921a41c9640cc1dc73345ff4cb9eb70f4719
senteu27 <hosomiakashi1227@icloud.com>
2025-12-10 13:35:42 +0900

    確認してうまくいかなかった部分の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          
          has_many :prototypes
          has_many :comments
+         validates :name, presence: true
+         validates :profile, presence: true
+         validates :occupation, presence: true
+         validates :position, presence: true
 end
